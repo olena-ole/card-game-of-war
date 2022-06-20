69965c22a5fe433c5b6230eb3fc80327d5ab7fd6
Lenna <pampam32121@gmail.com>
2022-06-20 21:28:44 +0300

    aadded defineWinner fn and additional stringToNum fn

--- index.js
+++ index.js
@@ -19,14 +19,41 @@ function renderCards(arr) {
     };
 };
 
+function stringToNum(str) {
+    switch (str) {
+        case "ACE":
+            return 14;
+        case "KING":
+            return 13;
+        case "QUEEN":
+            return 12;
+        case "JACK":
+            return 11;
+        default:
+            return +str;
+    };
+};
+
+function defineWinner(arr) {
+    const card1 = stringToNum(arr[0].value);
+    const card2 = stringToNum(arr[1].value);
+    if (card1 > card2) {
+        console.log('Computer wins!');
+    } else if (card2 > card1) {
+        console.log('You win!');
+    } else {
+        console.log('A Draw!');
+    };
+};
+
 function drawNewCards() {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards);
+            defineWinner(data.cards);
             renderCards(data.cards);
         });
-}
+};
 
 newDeckBtn.addEventListener('click', getNewDeck);
 drawBtn.addEventListener('click', drawNewCards);
