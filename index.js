926a96576d8966312c1d161c41e2786410f20e88
Lenna <pampam32121@gmail.com>
2022-06-20 17:05:44 +0300

    display two cards after pressing the Draw btn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 let deckId;
 const newDeckBtn = document.getElementById('new-deck');
 const drawBtn = document.getElementById('draw');
+const cardsWrapper = document.getElementById('cards-wrapper');
 
 function getNewDeck() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -10,10 +11,20 @@ function getNewDeck() {
         .then(data => deckId = data.deck_id);
 };
 
+function renderCards(arr) {
+    let cardsHtml = '';
+    for (let card of arr) {
+        cardsHtml += `
+            <img src="${card.image}" alt ="">
+        `
+    }
+    cardsWrapper.innerHTML = cardsHtml;
+}
+
 function drawNewCards() {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => renderCards(data.cards));
 }
 
 newDeckBtn.addEventListener('click', getNewDeck);
