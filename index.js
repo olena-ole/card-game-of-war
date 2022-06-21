28ea524f510d3e246104e1cb014f946c74382593
Lenna <pampam32121@gmail.com>
2022-06-22 00:18:59 +0300

    displayed score of players

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict';
 
 let deckId;
+let compScore = 0;
+let myScore = 0;
 const newDeckBtn = document.getElementById('new-deck');
 const drawBtn = document.getElementById('draw');
 const cardSlots = document.querySelectorAll('.card-slot');
 const resultTitle = document.getElementById('result-tite');
-const remainingCards = document.getElementById('remaining-cards')
+const remainingCards = document.getElementById('remaining-cards');
+const compScoreEl = document.getElementById('comp-score');
+const myScoreEl = document.getElementById('my-score');
+
+function displayScore() {
+    compScoreEl.textContent = `Computer: ${compScore}`;
+    myScoreEl.textContent = `Me: ${myScore}`;
+};
 
 function getNewDeck() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -14,6 +23,7 @@ function getNewDeck() {
             deckId = data.deck_id;
             remainingCards.textContent = `Remaining cards: ${data.remaining}`;
             drawBtn.disabled = false;
+            displayScore();
         });
 };
 
@@ -44,8 +54,10 @@ function defineWinner(arr) {
     const card1 = stringToNum(arr[0].value);
     const card2 = stringToNum(arr[1].value);
     if (card1 > card2) {
+        compScore++;
         resultTitle.textContent = 'Computer wins!';
     } else if (card2 > card1) {
+        myScore++;
         resultTitle.textContent = 'You win!';
     } else {
         resultTitle.textContent = "War!";
@@ -58,6 +70,7 @@ function drawNewCards() {
         .then(data => {
             defineWinner(data.cards);
             renderCards(data.cards);
+            displayScore();
             remainingCards.textContent = `Remaining cards: ${data.remaining}`;
 
             if (data.remaining === 0) {
